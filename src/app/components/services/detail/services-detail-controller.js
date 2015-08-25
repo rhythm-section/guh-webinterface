24abf1721e43971468bb60065d5479b88b9c9b8c
Lukas Mayerhofer <info@lukasmayerhofer.at>
2015-08-25 20:42:10 +0200

    Added basic detail view.

--- src/app/components/services/detail/services-detail-controller.js
+++ src/app/components/services/detail/services-detail-controller.js
@@ -39,8 +39,156 @@
     .module('guh.services')
     .controller('ServicesDetailCtrl', ServicesDetailCtrl);
 
-  ServicesDetailCtrl.$inject = ['$log'];
+  ServicesDetailCtrl.$inject = ['$log', '$scope', '$state', '$stateParams', 'libs', 'DSDevice', 'DSState'];
 
-  function ServicesDetailCtrl($log) {}
+  function ServicesDetailCtrl($log, $scope, $state, $stateParams, libs, DSDevice, DSState) {
+
+    var vm = this;
+    var currentService = {};
+
+    // Public methods
+    vm.remove = remove;
+
+
+    /**
+     * @ngdoc interface
+     * @name _loadViewData
+     * @methodOf guh.services.controller:ServicesDetailCtrl
+     *
+     * @description
+     * Set data for view.
+     * 
+     * @param {boolean} bypassCache True if service should be requested from Server instead of application memory (datastore)
+     *
+     */
+
+    function _loadViewData(bypassCache) {
+      var serviceId = $stateParams.serviceId;
+
+      // Return to service list if deviceId is unknown (e.g. on browser reload services/detail)
+      if(!serviceId) {
+        $state.go('guh.services.master');
+        return;
+      }
+
+      _findDevice(bypassCache, serviceId)
+        .then(function(service) {
+          currentService = service;
+
+          $log.log('currentService', currentService);
+
+          // Set view variables
+          vm.setupComplete = currentService.setupComplete;
+          vm.actions = [];
+          vm.deviceClass = currentService.deviceClass;
+          vm.deviceClassId = currentService.deviceClassId;
+          vm.id = currentService.id;
+          vm.name = (currentService.name === 'Name') ? currentService.deviceClass.name : currentService.name;
+          vm.params = currentService.params;
+          vm.states = currentService.states;
+
+          // Wait for templateUrl check
+          service.deviceClass.templateUrl
+            .then(function(fileExists) {
+              vm.templateUrl = fileExists.replace('devices', 'services');
+            })
+            .catch(function(error) {
+              $log.error('guh.controller.ServicesDetailCtrl', error);
+            })
+            .finally(function() {
+              vm.templateReady = true;
+            });
+
+          // Actions
+          angular.forEach(vm.deviceClass.actionTypes, function(actionType) {
+            var action = {};
+            action.actionType = actionType;
+
+            if(actionType.hasState) {  
+              var state = libs._.find(vm.deviceClass.stateTypes, function(stateType) {
+                return stateType.id === actionType.id;
+              });
+
+              // Add state to action
+              action.state = state;
+
+              // Remove state from sates array
+              vm.states = libs._.without(vm.states, state);
+            }
+
+            vm.actions.push(action);
+          });
+
+          // Subscribe to websocket messages
+          _subscribeToWebsocket();
+        })
+        .catch(function(error) {
+          $log.error('guh.controller.DevicesDetailCtrl', error);
+        });
+    }
+
+
+    /**
+     * @ngdoc interface
+     * @name _findDevice
+     * @methodOf guh.services.controller:ServicesDetailCtrl
+     *
+     * @description
+     * Load configured service
+     * 
+     * @param {boolean} bypassCache True if service should be requested from Server instead of application memory (datastore)
+     * @param {String} deviceId ID of the service that should be displayed
+     * @returns {Array} Promise of DSDevice Object
+     *
+     */
+
+    function _findDevice(bypassCache, deviceId) {
+      if(bypassCache) {
+        return DSDevice.find(deviceId, { bypassCache: true });
+      }
+      
+      return DSDevice.find(deviceId);
+    }
+
+    function _subscribeToWebsocket() {
+      currentService.subscribe(function(message) {
+        if(angular.isDefined(message.params.deviceId) && message.params.deviceId === vm.id) {
+          DSState.inject([{
+            stateTypeId: message.params.stateTypeId,
+            value: message.params.value
+          }]);
+        }
+      });
+    }
+
+    function _leaveState() {
+      // Unsubscribe websocket connection when leaving this state
+      if(DSDevice.is(currentService)) {
+        currentService.unsubscribe(currentService.id);
+      }
+    }
+
+    function remove() {
+      currentService
+        .remove()
+        .then(function(response) {
+          $log.log('Device succesfully removed');
+        })
+        .catch(function(error) {
+          // TODO: Build general error handler
+          // TODO: Handle error when service in use (rules)
+          $log.error(error);
+        });
+    }
+
+    $scope.$on('$stateChangeStart', function() {
+      _leaveState();
+    });
+
+
+    // Initialize controller
+    _loadViewData();
+
+  }
 
 }());
