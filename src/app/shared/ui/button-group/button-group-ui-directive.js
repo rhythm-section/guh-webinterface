69b3992771a211de552db1d927c0bbfb7a9d7d53
Lukas Mayerhofer <info@lukasmayerhofer.at>
2015-09-27 14:05:26 +0200

    Added button-group directive and enhanced hue light template.

---
+++ src/app/shared/ui/button-group/button-group-ui-directive.js
@@ -0,0 +1,163 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                     *
+ * Copyright (C) 2015 Lukas Mayerhofer <lukas.mayerhofer@guh.guru>                     *
+ *                                                                                     *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy        *
+ * of this software and associated documentation files (the "Software"), to deal       *
+ * in the Software without restriction, including without limitation the rights        *
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell           *
+ * copies of the Software, and to permit persons to whom the Software is               *
+ * furnished to do so, subject to the following conditions:                            *
+ *                                                                                     *
+ * The above copyright notice and this permission notice shall be included in all      *
+ * copies or substantial portions of the Software.                                     *
+ *                                                                                     *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR          *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,            *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE         *
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER              *
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,       *
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE       *
+ * SOFTWARE.                                                                           *
+ *                                                                                     *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+(function() {
+  'use strict';
+
+  angular
+    .module('guh.ui')
+    .directive('guhButtonGroup', guhButtonGroup);
+
+    guhButtonGroup.$inject = ['$log', 'DSDevice', 'DSActionType'];
+
+    function guhButtonGroup($log, DSDevice, DSActionType) {
+
+      var directive = {
+        bindToController: {
+          action: '=',
+          buttons: '=',
+          deviceId: '@'
+        },
+        controller: buttonGroupCtrl,
+        controllerAs: 'buttonGroup',
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'app/shared/ui/button-group/button-group.html'
+      };
+
+      return directive;
+
+
+      function buttonGroupCtrl() {
+
+        /* jshint validthis: true */
+        var vm = this;
+        var device = {};
+        var buttonParamType = {};
+
+        /*
+         * Public variables
+         */
+        vm.error = false;
+
+        /*
+         * Public methods
+         */
+        vm.pressButton = pressButton;
+
+
+        function _init() {
+          _checkParameters();
+
+          if(!vm.error) {
+            _setDevice();
+            _setButtonParamType();
+          }
+        }
+
+        function _checkParameters() {
+          // Only defined parameters and parameters without '?' are inside vm
+          angular.forEach(vm, function(value, key) {
+            if(angular.isDefined(value)) {
+              switch(key) {
+                case 'action':
+                  if(!angular.isObject(vm.action)) {
+                    $log.error('guh.ui.buttonGroupCtrl:controller | The value of parameter action has to be an object.');
+                    vm.error = true;
+
+                    if(angular.isUndefined(vm.action.actionType) || !DSActionType.is(vm.action.actionType)) {
+                      $log.error('guh.ui.buttonGroupCtrl:controller | The parameter action has to include an actionType.');
+                      vm.error = true;
+                    }
+                  }
+                  break;
+
+                case 'buttons':
+                  if(!angular.isArray(vm.buttons)) {
+                    $log.error('guh.ui.buttonGroupCtrl:controller | The value of parameter buttons has to be an array.');
+                    vm.error = true;
+                  }
+                  break;
+
+                case 'deviceId':
+                  if(!angular.isString(vm.deviceId)) {
+                    $log.error('guh.ui.buttonGroupCtrl:controller | The value of parameter deviceId has to be a string.');
+                    vm.error = true;
+                  }
+                  break;
+              }
+            } else {
+              vm.error = true;
+              $log.error('guh.ui.buttonGroupCtrl:controller | Parameter "' + key + '" has to be set.');
+            }
+          });
+        }
+
+        function _setDevice() {
+          device = DSDevice.get(vm.deviceId);
+        }
+
+        function _setButtonParamType() {
+          var paramType = vm.action.actionType.paramTypes[0];
+
+          if(angular.isDefined(paramType) && angular.isDefined(paramType.allowedValues)) {
+            buttonParamType = angular.copy(paramType);
+          }
+        }
+
+        function pressButton(button) {
+          var params = [];
+
+          // Set params
+          if(angular.isArray(buttonParamType.allowedValues)) {
+            angular.forEach(buttonParamType.allowedValues, function(allowedValue) {
+              if(allowedValue === button) {
+                params.push({
+                  name: buttonParamType.name,
+                  value: allowedValue
+                });
+              }
+            });
+          }
+
+          // Execute action
+          if(angular.isDefined(device) && DSDevice.is(device)) {
+            device.executeAction(vm.action.actionType, params)
+              .then(function(response) {
+                $log.log('Action executed', response);
+              })
+              .catch(function(error) {
+                $log.log('error', error);
+              });
+          }
+        }
+
+        _init();
+
+      }
+
+    }
+
+}());
