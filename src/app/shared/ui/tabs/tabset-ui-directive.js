28ad2a3855e6a7b11466414adc6dddf60085d9c8
Lukas Mayerhofer <info@lukasmayerhofer.at>
2015-10-21 18:17:20 +0200

    Added hotkeys to tabset directive.

--- src/app/shared/ui/tabs/tabset-ui-directive.js
+++ src/app/shared/ui/tabs/tabset-ui-directive.js
@@ -29,9 +29,9 @@
     .module('guh.ui')
     .directive('guhTabset', guhTabset);
 
-    guhTabset.$inject = [];
+    guhTabset.$inject = ['$log', 'hotkeys'];
 
-    function guhTabset() {
+    function guhTabset($log, hotkeys) {
       var directive = {
         controller: tabsetCtrl,
         controllerAs: 'tabset',
@@ -45,7 +45,8 @@
       return directive;
 
 
-      function tabsetCtrl() {
+      function tabsetCtrl($scope) {
+        
         /* jshint validthis: true */
         var vm = this;
 
@@ -61,11 +62,36 @@
         vm.selectTab = selectTab;
 
         function addTab(tab) {
+          var hotkey = tab.heading.charAt(0).toLowerCase();
+
           vm.tabs.push(tab);
 
           // Set first tab active
           if(vm.tabs.length === 1) {
             tab.active = true;
+          }
+
+          // Bind hotkey only if combo is letter (a-z)
+          if(hotkey.match(/[a-z]/)) {
+            tab.hotkey = true;
+
+            hotkeys
+              .bindTo($scope)
+              .add({
+                combo: hotkey,
+                description: tab.heading,
+                callback: function(event, hotkey) {
+                  event.preventDefault();
+
+                  angular.forEach(vm.tabs, function(tab) {
+                    if(tab.heading === hotkey.description) {
+                      selectTab(tab);
+                    }
+                  });
+                }
+              });
+          } else {
+            tab.hotkey = false;
           }
         }
 
